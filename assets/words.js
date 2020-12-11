f92ca52c95fc25333259788d3a39fa1e0de2c365
Gypsophily818 <714209954@qq.com>
2020-12-11 21:49:28 +0800

    site

---
+++ assets/words.js
@@ -0,0 +1,338 @@
+const myword = [
+	
+	{
+		"word": "biodiversity",
+		"means": "生物多样性",
+		"eg": "Biodiversity was the buzzword of the Rio Earth Summit.生物多样性是里约地球峰会上的时髦用语。"
+	},
+	{
+		"word": "interpretation",
+		"means": "解释",
+		"eg": "无例句"
+	},
+	{
+		"word": "intervention",
+		"means": "n. 插入, 介入, 调停 ; [经] 干预",
+		"eg": "1:Many people felt he would be hostile to the idea of foreign intervention.许多人觉得他会反对外来干预。"
+	},
+	{
+		"word": "invention",
+		"means": "发明",
+		"eg": "无例句"
+	},
+	{
+		"word": "observation",
+		"means": "n. 观察, 注意, 观测, 观察力 ; [化] 观察",
+		"eg": "The diaries are a mixture of confession and observation.这些日记混合着自白和一些观察。"
+	},
+	{
+		"word": "comprehensive",
+		"means": "综合性的，广泛的",
+		"eg": "无例句"
+	},
+	{
+		"word": "laborer",
+		"means": "n. 体力劳动者, 辅助工",
+		"eg": "1:Prior to the implementation hereof, an employing unit shall make economic compensation to the laborer in accordance with the prevailing provisions.本法施行前按照当时有关规定，用人单位应当向劳动者支付经济补偿的，按照当时有关规定执行。"
+	},
+	{
+		"word": "corporation",
+		"means": "n. 公司, 合作, 法人团体 ; [法] 法人团体, 社团, 法人 ",
+		"eg": "The rule applies where a person owns stock in a corporation.该规定对于在公司里拥有股份的人适用。"
+	},
+	{
+		"word": "graduation",
+		"means": "毕业；毕业典礼；刻度，分度；分等级",
+		"eg": "My whole family came to my graduation.我的家人都来参加了我的毕业典礼。"
+	},
+	{
+		"word": "estimate",
+		"means": "估计",
+		"eg": "my estimate was bang on target"
+	},
+	{
+		"word": "regime",
+		"means": "政权，政体；社会制度；管理体制",
+		"eg": "fitness regime:健身计划"
+	},
+	{
+		"word": "transaction",
+		"means": "n. 交易, 办理, 学报, 和解协议 ; [计] 事务处理 ",
+		"eg": "The transaction is completed by payment of the fee.交易在支付费用之后就完成了。"
+	},
+	{
+		"word": "economist",
+		"means": "经济学家",
+		"eg": "无例句"
+	},
+	{
+		"word": "obesity",
+		"means": "n. 肥胖 ; [医] 肥胖, 多脂",
+		"eg": "Obesity can increase the risk of heart disease.肥胖会增加患心脏病的危险。"
+	},
+	{
+		"word": "poll ",
+		"means": "n. 投票, 民意测验, 选举投票, 投票数, 一组人中的一个, 头颈和后脑部, 鹦鹉 ; vt. 对...进行民意测验, 获得...票, 剪树枝, 轮询 ; vi. 投票 ; a. 剪过毛的, 修过枝的 ; [计] 轮询 ;",
+		"eg": "A national poll shows the presidential race in a dead heat.一项全国民意测验显示总统竞选不分胜负。"
+	},
+	{
+		"word": "pretext",
+		"means": "n.借口,托辞",
+		"eg": "无例句"
+	},
+	{
+		"word": "console",
+		"means": "vt.安慰，抚慰，慰藉机器设备的控制台、仪表盘",
+		"eg": "无例句"
+	},
+	{
+		"word": "decisive",
+		"means": "决定性的、关键的、果断的",
+		"eg": "无例句"
+	},
+	{
+		"word": "prone",
+		"means": "有...倾向的",
+		"eg": "无例句"
+	},
+	{
+		"word": "propel",
+		"means": "v.推动、激励",
+		"eg": "what is out there that is unknown can propel us to do more.未知的事情能驱使我们做更多的事"
+	},
+	{
+		"word": "kidnap",
+		"means": "v. 劫持; 绑架;",
+		"eg": "无例句"
+	},
+	{
+		"word": "fertilizer",
+		"means": "词义",
+		"eg": "无例句"
+	},
+	{
+		"word": "resistant",
+		"means": "n.电阻、抵抗力",
+		"eg": "无例句"
+	},
+	{
+		"word": "temperament",
+		"means": "气质、性情、秉性",
+		"eg": "无例句"
+	},
+	{
+		"word": "conventional ",
+		"means": " 传统的, 习惯的, 约定的 ; [经] 惯例的, 常规的, 传统的 ",
+		"eg": "Biofuels can be mixed with conventional fuels.生物燃料能够和传统燃料混合。"
+	},
+	{
+		"word": "personal ",
+		"means": "私人的, 涉及隐私的, 有人性的, 人称的, 亲自的, 身体的 ; [医] 人的; 个人的, 自身的",
+		"eg": "Do you have a personal trainer?你有私人教练吗？"
+	},
+
+	/* ------------------------------------------------------------------------------------ */
+	/* -----------------------------------JSON格式开始------------------------------------- */
+	/* ---------------------------------2020 / 11 / 13------------------------------------- */
+	{
+		"word" : "ambiguous",
+		"means" : "adj.模棱两可的,含糊不清的",
+		"eg" : "There's a lot of ambiguous language in this contract.这份合同有太多模糊用语了。"
+	},
+	{
+		"word" : "disregard",
+		"means" : "vt.不理会,不顾,忽视",
+		"eg" : "You chose to disregard my warnings.你选择了无视我的警告。"
+	},
+	{
+		"word" : "invisible",
+		"means" : "不可见的",
+		"eg" : "无例句"
+	},
+	{
+		"word" : "quest",
+		"means" : "n.探索,寻求,追求(幸福等) vi.探索，探求",
+		"eg" : "无例句"
+	},
+	{
+		"word" : "submerge",
+		"means" : "vt vi.(使)浸没,淹没 vt.掩盖,遮掩(想法、感情等)",
+		"eg" : "无例句"
+	},
+	{
+		"word" : "discolse",
+		"means" : "vt. 揭露,泄露;使显露",
+		"eg" : "无例句"
+	},
+	{
+		"word" : "priest",
+		"means" : "牧师",
+		"eg" : "Base on a true story,"
+	},
+	{
+		"word" : "justification",
+		"means" : "n. 正当理由,合理解释",
+		"eg" : "That's your justification for killing tow kids?"
+	},
+	{
+		"word" : "temper",
+		"means" : "n. 坏脾气;怒气;心情 vt. 使缓和;使(金属)回火",
+		"eg" : "Ah,the Starks...quick tempers,slow minds.史塔克家啊...净是暴脾气,死脑筋."
+	},
+	{
+		"word" : "assert",
+		"means" : "vt. 坚持;坚称",
+		"eg" : "无例句"
+	},
+	// 11-14
+	{
+		"word" : "诶...有一个说是我在健身房练功颈椎练坏了，马老师你能不能教教我浑元功法，诶...帮助治疗一下，我的颈椎病。我说可以。",
+		"means" : "词义",
+		"eg" : "无例句"
+	},
+	{
+		"word" : "单词",
+		"means" : "词义",
+		"eg" : "无例句"
+	},
+	{
+		
+	}
+]
+var str = `网络的应用提高效率
+
+For individuals, the pervasive use of the Internet helps people save time and enhance the efficiency.
+So convenient is the application of telecommunication and online-shopping that [例证原因] people are extricating themselves from such undesirable factors as traffic congestion, bad weather and tight schedule and so forth.
+经济发展、市场繁荣
+
+In terms of international trade, exporting countries may become major beneficiaries of this situation, because it helps [扩大市场]expand the market of farm produce, [加速销售]accelerate the sales of agricultural output and [改善福祉]improve the well-being of domestic farmers.
+[工业受益] For society, industries such as electronic commerce and online-business have become major beneficiaries of this tendency.
+[原因句]This is simply because the interaction and communication are [地理不受限]no longer confined to the geographical impediment.
+[前因后果句] With the increasing popularity of [线上支付兴起]online shopping and banking, [投资消费增长]consumption and investment undoubtedly flourish, which can serve as a driving factor to [加速经济建设]facilitate the sustainable development of the social economy.
+经济倒退、破产失业
+
+However, many small and medium-sized enterprises may [中小企业成为受害者]fall victim to the wave of electronic business [原因后置]on account of the [资源不足以开展线上业务]insufficiency of money and technology to develop an online transaction system.
+Also, [失业率激增] the unemployment rate may be driven up [原因后置] because many job positions [工作被替代]have and will continue to be replaced by online automatic transaction systems.
+教育类 - 学校职能
+
+[学校职能] To begin with, schools are set up to fulfill the educational function of imparting a great deal amount of knowledge, such as liberal arts and science, to students; while students go to school mainly for the purpose of fostering academic ability.
+Accordingly, it is understandable that some take it for granted that academic results can serve as an effective benchmark for assessing students’ capability.
+教育类 - 学校培养多样化能力
+
+[正反对比] First and foremost, schoolsare not a place[成为书呆子] where students are turned into pedants. Instead, it is considered as an educational platform for students to [培养多样化才能]develop their versatile skills and [满足社会需求]grow into a person who gets accustomed to the needs of the society.
+Only focusing on academic skills inevitably leads many who are short of academic study but good at other subjects to lose opportunities to promote their talents.
+教育类 - 用成绩选拔学生公平
+
+Besides this, it is widely accepted that it [客观公正的评估系统] is a relatively objective and fair assessment system for all students and [减少不公正现象] better reduces the dispute of unfair treatments.
+[例证]One example will suffice to illustrate this point.Other than the academic mark, educators find it difficult to compare the ability of a student who is proficient at painting and who is skillful at sports. [例证结论] Whoever is chosen by schools could not ensure the absolute fairness and objectivity of the selection.
+教育类 - 不同能力学生互补
+
+In addition, if students with different abilities can study together, it is out of the question that[学生受益] all the students will benefit from such a combination.
+[原因句]This is simply because it helps students [互补]complement each other and give them motivation as well as chances to [互相学习]learn more from each other.
+网络类 - 沉迷网络
+
+However, over-indulgence on the Internet potentially [沦为奴隶]renders people as a slave of the Internet and computer.
+[前因后果句] Due to the [缺乏面对面交流]absence of face-to-face interview and communication, [地理与心灵疏离] people are going to be, geographically as well as emotionally, isolated from each other, which further makes people [失去认同感和社会归属感]lose a sense of identity and social belonging.
+健康类 - 丰富膳食、增加营养
+
+For importing countries, those agricultural products substantiallyenrich the diet of local citizens and improve the nutrition structure.
+环境类 - 加速化石燃料损耗
+
+[主题句] To begin with, it accelerates the depletion of fossil fuels that are non-renewable resources.
+[解释上一句] So far there has not been a complete substitution for oil.
+[建议性结局] We should not pursue short-term profit at the expense of sustainable development.
+环境类 - 有毒气体排放
+
+[有毒气体] Also, the toxic fumes - such as sulfur dioxide, released from the aircraft are the chief culprit of atmospheric contamination.
+[结果句] Consequently, people are unavoidably afflicted with respiratory diseases.
+[温室气体]Other than toxic gases, greenhouse gases (i.e. carbon dioxide) worsen global warming and then potentially induce the change of global climate.
+大家可以感受一下上面的这些句子，应该不难发现基本上每一个句子开头都有一个衔接词，并且每个句子都习惯使用多个名词、动词或形容词并列来进行句子描述。以及这些句子都充斥着大量的词组与各类句型，大家可以记忆一下里面的句型，会在考试中发挥作用的～
+
+最后祝大家都能拿到四六级写作和翻译高分～（并且听力也不会拉垮...
+
+二更！感谢大家的赞，我来给大伙们加点好句子！（我觉得用上肯定加分！
+
+对了，提醒一下大家，四六级翻译部分的中文内容是没有衔接词的，但是大家翻译成英文的时候一定要记得加上！
+
+正面句
+1. Play a significant role in（在...方面起重大作用）
+
+Vitamins and minerals play a significant role in keeping our body healthy; therefore, children and adults alike should eat more vegetables that are rich in various vitamins and minerals.
+
+2. A has a strong influence on B（A 对 B 有重大影响）
+
+Visiting museums, historical sites and ruins could have a strong influence on deepening visitors' understanding in terms of local customs and practices, enriching their experiences concerning local traditions, art and heritages, and promoting the dialogue over natural and cultural resources between different countries.
+
+3. A could be highly beneficial to B（B 从 A 获益）
+
+The large amount of investment on education could be highly beneficial to the development of economy and society.
+
+上述三句常用于连接名词和动词，可与后面的句子搭配使用。
+4. Promote the development / growth of B（促进 B 发展）
+
+Even though the city invested millions of pounds in museums and art galleries, it had done little to promote the development of education and medical care.
+
+5. There is a close relationship between A and B（A 和 B 有密切的关系）
+
+There is a close relationship between the rising rate of obesity and the increasing consumption of fast food.
+
+6. Relax their bodies and ease their minds（放松身心）
+
+Children not only need opportunities to acquire knowledge, they need time to relax their bodies and ease their minds well.
+
+7. Enrich one's social / professional / spiritual experience（丰富...经历）
+
+Working for a period before going to university can greatly enrich student's social experience and make them more capable in university.
+
+8. Improve living standard of citizens（提高市民生活水平）
+
+The development of technology and economy play a significant role in improving living standard of citizens.
+
+9. Enhance efficiency / productivity（提高效率 / 生产率）
+
+The main question about high-tech education is whether it actually enhances the efficiency and the effectiveness of learning.
+
+10. Relieve one's academic / financial / psychological burden（减轻某人的学业 / 经济 / 心理负担）
+
+Part-time job help students to relieve their parents' burdens, nevertheless, working on a part-time basis may create psychological burdens for the students themselves.
+
+负面句
+1. Lead to = Result in = bring about = Give rise to（导致）
+
+The trend that population has been booming has resulted in the deterioration of natural environment.
+
+2. Have a negative / serious influence on（对...有负面影响）
+
+Apparently, long-distance flights have serious influence upon the environment, even though they are by far the most efficient way of traveling.
+
+3. The competition is increasingly stiff（竞争日益激烈）
+
+The economic competition between countries is increasingly stiff, which has made the governments more protective of their domestic market.
+
+4. Live in poverty / misery / frustration / anxiety（穷 / 苦 / 沮丧 / 焦虑）
+
+Living in misery, the citizens in these impoverished countries claim that industrial nations have the obligation to supply financial aid, which can lift them out of poverty.
+
+5. Stem from ...（某种坏现象起源于...）
+
+Generally speaking, children's bad behavior stems from imitating their parents.
+
+6. Pose threat to personal health / security / environment（对...产生危害）
+
+The weapons that a country possesses do not just pose a threat to other countries. They pose a very real threat to their owner as well.
+
+7. Be overly dependent on（对...过度依赖）
+
+Technology has made individual overly dependent on it, which has led to more sedentary lifestyle and less face-to-face communication.
+
+8. Stifle creativity / imagination（扼杀创造力 / 想象力）
+
+The traditional approach to education in some Asian countries seriously stifles students' creativity and imagination.
+
+9.Has long been criticized as（臭名昭著）
+
+It is widely accepted that international fast foods have long been criticized as junk food because they contain a considerable number of calories with high level of heat and fat.
+
+10. Face the problem of（面对...问题）
+
+So a new challenge was raised: how to face the problem of excessive information in theso-called era ofinformation exploding.`
